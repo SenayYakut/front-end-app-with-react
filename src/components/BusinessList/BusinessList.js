21c6756af8fdd14aad0334603186e0ef0c2d8916
Senay Yakut <senaykt@gmail.com>
2021-07-28 14:42:00 -0700

    Add related import and export scripts

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,35 +1,21 @@
-import React from "react";
-import "./BusinessList";
+import React from 'react';
+import './BusinessList.css';
+
 import Business from '../Business/Business';
 
-
-class BusinessList extends React.Components {
-render() {
-    return ( < div className = "BusinessList" >
-        <
-        Business / >
-        <
-        Business / >
-        <
-        Business / >
-        <
-        Business / >
-        <
-        Business / >
-        <
-        Business / >
-
-
-
-
-
-
-
-        <
-        /div>
-    )
+class BusinessList extends React.Component {
+  render() {
+    return (
+      <div className="BusinessList">
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+      </div>
+    );
+  }
 }
-};)
-}
-};
+
 export default BusinessList;
